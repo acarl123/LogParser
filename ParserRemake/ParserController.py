e4266a98a4da7bc281cec545ac1899b3fada17f4
Andrew Carl <chuckles.theman@gmail.com>
2015-01-09 16:49:19 -0500

    Excel reporting

--- ParserRemake/ParserController.py
+++ ParserRemake/ParserController.py
@@ -2,7 +2,11 @@ from collections import defaultdict, Counter
 from ParserView import LogView
 from ParserDetailsController import ParserDetailsController
 import wx
+import time
 import os, sys, re, threading
+
+sys.path.append(r'c:\hg\tools_lag\EFS_Utils')
+import Excel
 
 
 openStartKeyword = "perf: teamcenter -> open begins"
@@ -39,13 +43,21 @@ class MainController:
 
       # Bind events
       self.mainWindow.LogFileListCtrl.Bind(wx.EVT_LIST_KEY_DOWN, self.onDelete)
-      self.mainWindow.btnAddFiles.Bind(wx.EVT_BUTTON, self.onFile)
       self.mainWindow.calcButton.Bind(wx.EVT_BUTTON, self.onCalc)
       self.mainWindow.clearButton.Bind(wx.EVT_BUTTON, self.onClear)
       self.mainWindow.LogFileListCtrl.Bind(wx.EVT_CONTEXT_MENU, self.onRClick)
       self.mainWindow.LogFileListCtrl.Bind(wx.EVT_DROP_FILES, self.onFile)
       self.mainWindow.displaySummaryListCtrl.Bind(wx.EVT_LIST_COL_RIGHT_CLICK, self.onColRClick)
+
+      # Bind menu events
+      self.mainWindow.Bind(wx.EVT_MENU, self.onFile, self.mainWindow.menuAdd)
+      self.mainWindow.Bind(wx.EVT_MENU, self.exit, self.mainWindow.menuExit)
+      self.mainWindow.Bind(wx.EVT_MENU, self.onExport, self.mainWindow.menuExport)
+
       self.dirPaths = []
+
+   def exit(self, event):
+      sys.exit()
 
    def show(self, *args):
       self.mainWindow.Show()
@@ -114,6 +126,8 @@ class MainController:
       self.mainWindow.SetCursor(wx.StockCursor(wx.CURSOR_WAIT))
       self.onClear()
       self.initList()
+      self.startTime = time.clock()
+      self.mainWindow.m_statusBar1.SetStatusText('Calculating...')
       threads = []
       for fileIndex in xrange(self.mainWindow.LogFileListCtrl.GetItemCount()):
          logFileName = os.path.join(self.dirPaths[fileIndex], self.mainWindow.LogFileListCtrl.GetItemText(fileIndex))
@@ -262,48 +276,84 @@ class MainController:
 
       return opTimeDict, opCountDict, logFileName, timelineDict
 
-   def populateList(self, times, count, colName, timeInfo={}):
-      curCol = self.mainWindow.displaySummaryListCtrl.ColumnCount
-      self.mainWindow.displaySummaryListCtrl.InsertColumn(curCol, os.path.basename(colName))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(0, curCol, str(times['integration']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(1, curCol, str(times['teamcenter']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(2, curCol, str(times['user']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(3, curCol, str(times['download']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(4, curCol, str(times['totalOp']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(5, curCol, str(times['totalOpNoUser']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(6, curCol, str(count['save']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(7, curCol, str(count['open']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(8, curCol, str(count['manage']))
-      self.mainWindow.displaySummaryListCtrl.SetStringItem(9, curCol, str(count['total']))
+   def populateList(self, times, count, fileName, timeInfo={}):
+      index = self.mainWindow.displaySummaryListCtrl.GetItemCount()
+      self.mainWindow.displaySummaryListCtrl.InsertStringItem(index, str(os.path.basename(fileName)))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 1, str(times['integration']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 2, str(times['teamcenter']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 3, str(times['user']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 4, str(times['download']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 5, str(times['totalOp']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 6, str(times['totalOpNoUser']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 7, str(count['save']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 8, str(count['open']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 9, str(count['manage']))
+      self.mainWindow.displaySummaryListCtrl.SetStringItem(index, 10, str(count['total']))
 
-      self.times[curCol] = times
-      self.count[curCol] = count
-      self.timelineDict[curCol] = timeInfo
+      self.times[fileName] = times
+      self.count[fileName] = count
+      self.timelineDict[fileName] = timeInfo
 
       if len(threading.enumerate()) <= 2:
          self.mainWindow.SetCursor(wx.StockCursor(wx.CURSOR_ARROW))
+         self.mainWindow.m_statusBar1.SetStatusText('%s files calculated in %.2f seconds' % (self.mainWindow.displaySummaryListCtrl.GetItemCount(), time.clock() - self.startTime))
 
-      # for some reason, altering width was the only way I could get the scroll bar on the bottom to update
-      if self.mainWindow.Size == (550, 520):
-         self.mainWindow.SetSize((549, 520))
-      else:
-         self.mainWindow.SetSize((550, 520))
+      self.mainWindow.SetSize((500, 520))
 
    def initList(self):
-      self.mainWindow.displaySummaryListCtrl.InsertColumn(0, '', width=150)
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(0, 'IPEM Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(1, 'Teamcenter Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(2, 'User Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(3, 'Download Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(4, 'Total Operation Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(5, 'Total Op w/o User Time')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(6, 'Total Save Ops')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(7, 'Total Open Ops')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(8, 'Total Manage Ops')
-      self.mainWindow.displaySummaryListCtrl.InsertStringItem(9, 'Total Ops')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(0, 'Filename')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(1, 'IPEM Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(2, 'Teamcenter Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(3, 'User Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(4, 'Download Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(5, 'Total Operation Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(6, 'Total Op w/o User Time')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(7, 'Total Save Ops')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(8, 'Total Open Ops')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(9, 'Total Manage Ops')
+      self.mainWindow.displaySummaryListCtrl.InsertColumn(10, 'Total Ops')
 
    def onClear(self, event=None):
       self.mainWindow.displaySummaryListCtrl.ClearAll()
+
+   def onExport(self, event):
+      fileDlg = wx.FileDialog(self.mainWindow, 'Choose a save file location', '', '', 'Excel files (*.xlsx)|*.xlsx', wx.FD_SAVE|wx.FD_OVERWRITE_PROMPT)
+
+      if fileDlg.ShowModal() == wx.ID_CANCEL: return
+
+      reportFile = fileDlg.GetPath()
+      wb = Excel.Workbook()
+      report = wb.create_worksheet('Integration log time report', columnWidths=[25, 20, 25, 20, 25, 30, 30, 25, 25, 25, 20])
+      # sets header cells
+      report.add_header('A1')
+      report.add_header('B1')
+      report.add_header('C1')
+      report.add_header('D1')
+      report.add_header('E1')
+      report.add_header('F1')
+      report.add_header('G1')
+      report.add_header('H1')
+      report.add_header('I1')
+      report.add_header('J1')
+      report.add_header('K1')
+      report.append(['Filename', 'IPEM Time', 'Teamcenter Time', 'User Time', 'Download Time', 'Total Operation Time', 'Total Op w/o User Time', 'Total Save Ops', 'Total Open Ops', 'Total Manage Ops', 'Total Ops'])
+      startcount = rowcounter = 1
+
+      for row in xrange(self.mainWindow.displaySummaryListCtrl.GetItemCount()):
+         rowcounter += 1
+         iteminfo = []
+         for col in xrange(self.mainWindow.displaySummaryListCtrl.GetColumnCount()):
+            iteminfo.append(self.mainWindow.displaySummaryListCtrl.GetItem(row, col).GetText())
+         report.append(iteminfo)
+
+      report.add_table('Table'+str(1), 'A%s:K%s' % (str(startcount), str(rowcounter)), ['Filename', 'IPEM Time', 'Teamcenter Time', 'User Time', 'Download Time', 'Total Operation Time', 'Total Op w/o User Time', 'Total Save Ops', 'Total Open Ops', 'Total Manage Ops', 'Total Ops'])
+
+      try:
+         wb.save(reportFile)
+         wx.MessageBox('File successfully saved.', 'Success!', wx.ICON_INFORMATION | wx.OK, self.mainWindow)
+      except IOError:
+         wx.MessageBox('The action cannot be completed because the file is in use.  Close the file and try again.',
+                       'File In Use', wx.ICON_ERROR | wx.OK, self.mainWindow)
 
 
 class Worker( threading.Thread ):
